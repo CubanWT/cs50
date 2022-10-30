c4574748afc96c682f27e27e7981a6ec28bafb22
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-10-30 01:37:48 +0000

    Sun, Oct 30, 2022, 1:37 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -8,9 +8,17 @@ int main(void)
 
     //Check if input checksum is valid
 
+
+
+
     int digit1 = input % 100;
     digit1 = (digit1 - (digit1 % 10)) / 10;
+
     printf("%i\n", digit1);
+
+
+
+
 
 
         //Check if mastercard, visa, AMEX,
