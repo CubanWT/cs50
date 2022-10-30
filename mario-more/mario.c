803e917b08c785edfaf338023e856fd2e7fe3229
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-10-30 01:07:37 +0000

    Sun, Oct 30, 2022, 1:07 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -6,41 +6,46 @@ int main(void)
 
     bool isInRange;
 
-do {
-    int height = get_int("Pyramid height: ");
-
-    if(height > 0 && height < 9)
+    do
     {
-        for(int row = 1; row <= height; row++)
+        int height = get_int("Pyramid height: ");
+
+        if (height > 0 && height < 9)
         {
-            //print spaces
-            int spaces = height - row;
-            for(int i = spaces; i > 0; i--) {
-                printf(" ");
+            for (int row = 1; row <= height; row++)
+            {
+                //print spaces
+                int spaces = height - row;
+                for (int i = spaces; i > 0; i--)
+                {
+                    printf(" ");
+                }
+
+                //print hashes
+                for (int i = row; i > 0; i--)
+                {
+                    printf("#");
+                }
+
+                //print middle space
+                printf("  ");
+
+                //print hashes
+                for (int i = row; i > 0; i--)
+                {
+                    printf("#");
+                }
+                printf("\n");
             }
 
-            //print hashes
-            for(int i = row; i > 0; i--) {
-                printf("#");
-            }
-
-            //print middle space
-            printf("  ");
-
-            //print hashes
-            for(int i = row; i > 0; i--) {
-                printf("#");
-            }
-            printf("\n");
+            isInRange = true;
         }
-
-        isInRange = true;
+        else
+        {
+            isInRange = false;
+        }
     }
-    else
-    {
-        isInRange = false;
-    }
-} while(!isInRange);
+    while (!isInRange);
 
 
 }
