50f2327e9547e7765ff7603d67c12ab84e15a2ba
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-10-30 02:19:23 +0000

    Sun, Oct 30, 2022, 2:19 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,35 +1,11 @@
 #include <cs50.h>
-#include <math.h>
 #include <stdio.h>
 
 int main(void)
 {
-
     //Get card input
     long input = get_long("Enter Credit Card Number: ");
 
+    //
 
-    //Check if input checksum is valid
-
-    int evenTotal;
-    bool stepComplete = false;
-    int digitPosition = 0;
-
-   do
-    {
-
-        digitPosition += 2;
-
-        long digit = input % pow(10, digitPosition);
-        digit = (digit - digit % pow(10, (digitPosition - 1))) / pow(10, (digitPosition - 1));
-
-        printf("digitPosition: %i, ",digitPosition);
-        printf("digit: %li\n", digit);
-        stepComplete = true;
-
-    } while (!stepComplete);
-
-
-    //Check if mastercard, visa, AMEX,
-    //else: output invalid
 }
