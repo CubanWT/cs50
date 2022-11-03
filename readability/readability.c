6711d4099a1799859db4c2f2ec3a222d7de04fe9
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-11-03 01:52:13 +0000

    Thu, Nov 3, 2022, 1:52 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,7 +3,16 @@
 
 int main(void)
 {
-    string input = get_string("Enter text: ");
+    string input = get_string("Text: ");
+    int i = 0, letters = 0, words = 0, sentences = 0;
 
-    
+    do
+    {
+        //count letters
+        if (input[i] > )
+
+        //cycle through chars in input
+        i++;
+    }
+    while (input[i] != '\0');
 }
