73c0364c0e9272418ebc2643c9fb1949dfd94356
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-11-03 03:40:31 +0000

    Thu, Nov 3, 2022, 3:40 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -5,20 +5,23 @@
 
 int main(int argc, string argv[])
 {
-    string key = argv[1];
-
-    // ensure key is the right length
-    if (strlen(key) == 0)
+    // ensure key is the right length and exists
+    if (!argv[1])
     {
         printf("Usage: ./substitution key\n");
         return 1;
     }
 
-    else if (strlen(key) != 26)
+    else if (strlen(argv[1]) != 26)
     {
         printf("Error! key is not 26 characters long!\n");
         return 1;
     }
+
+    string key = argv[1];
+
+    // get user input
+    string input = get_string("plaintext: ");
 
     // convert key to uppercase
     for (int i = 0; i < strlen(key); i++)
@@ -26,7 +29,7 @@ int main(int argc, string argv[])
         key[i] = toupper(key[i]);
     }
 
-    string input = get_string("plaintext: ");
+
 
 
 }
