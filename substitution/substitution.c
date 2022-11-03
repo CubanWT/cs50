3a9119d3c787eee9acc9a9b1f20ea2af00a08d91
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-11-03 03:21:49 +0000

    Thu, Nov 3, 2022, 3:21 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -5,9 +5,13 @@
 int main(int argc, string argv[])
 {
     string key = argv[1];
+
     if (strlen(key) != 26)
     {
         printf("Error! key is not 26 characters long!\n");
         return -1;
     }
+
+    string input = get_string("plaintext: ");
+    
 }
