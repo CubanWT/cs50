24902c0d0973920fbeeb01e222d6d3a6a5b456e4
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-11-03 02:03:08 +0000

    Thu, Nov 3, 2022, 2:03 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,16 +1,32 @@
 #include <cs50.h>
+#include <string.h>
 #include <stdio.h>
+
+
+int count_letters(string text);
 
 int main(void)
 {
     string input = get_string("Text: ");
-    int i = 0, letters = 0, words = 0, sentences = 0;
 
-
+    printf("%i letters\n", count_letters(input));
 
 }
 
 int count_letters(string text)
 {
-    
+    int letters = 0;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (text[i] >= 'A' && text[i] <= 'Z')
+        {
+            if (text[i] >= 'a' && text[i] <= 'z')
+            {
+                letters += 1;
+            }
+        }
+    }
+
+    return letters;
 }
