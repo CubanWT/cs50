2e888eadac32434fde0c4a326d4001903613dc95
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-11-03 03:20:00 +0000

    Thu, Nov 3, 2022, 3:20 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
+#include <string.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
-
+    string key = argv[1];
+    if (strlen(key) != 26)
+    {
+        printf("Error! key is not 26 characters long!\n");
+        return -1;
+    }
 }
