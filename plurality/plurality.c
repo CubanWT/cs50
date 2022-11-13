9ccca868b82f4a70f1c7ef10e195b02c622253b7
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-11-13 18:15:56 +0000

    Sun, Nov 13, 2022, 6:15 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -86,7 +86,10 @@ void print_winner(void)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        if(candidate[i].votes > candidate[i + 1].votes)
+        if (candidate[i].votes > candidate[i + 1].votes)
+        {
+            
+        }
     }
 
 
