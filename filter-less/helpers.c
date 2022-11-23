b77d3b7b9755d03139e15181d78f9a4f1be60571
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-11-23 19:59:24 +0000

    Wed, Nov 23, 2022, 7:59 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -56,7 +56,13 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i < height, i++)
+    {
+        for (int j = 0; j < width, j++)
+        {
+            
+        }
+    }
 }
 
 // Blur image
