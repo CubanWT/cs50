11cc7c72a314675e1fbd1670b444d9ac862e332b
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-11-25 07:52:26 +0000

    Fri, Nov 25, 2022, 7:52 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,7 +1,6 @@
 #include "helpers.h"
 #include <math.h>
 
-RGBTRIPLE rowAvg(int row, int column, int width, RGBTRIPLE *copy[row + 1][column + 1]);
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -87,62 +86,8 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE avgAbove, avgLine, avgBelow;
             copy[i][j] = image[i][j];
-
-            if (i == 0)
-            {
-                avgAbove = 0;
-                avgLine = rowAvg(i, j, width, &copy);
-                avgBelow = rowAvg(i + 1, j, width, &copy);
-            }
-            else if (i == height - 1)
-            {
-                avgAbove = rowAvg(i - 1, j, width, &copy);
-                avgLine = rowAvg(i, j, width, &copy);
-                avgBelow = 0;
-            }
-            else
-            {
-                avgAbove = rowAvg(i + 1, j, width, &copy);
-                avgLine = rowAvg(i, j, width, &copy);
-                avgBelow = rowAvg(i + 1, j, width, &copy);
-            }
-
-            image[i][j].rgbtRed = round((avgAbove.rgbtRed + avgLine.rgbtRed + avgBelow.rgbtRed) / 3);
-            image[i][j].rgbtBlue = round((avgAbove.rgbtBlue + avgLine.rgbtBlue + avgBelow.rgbtBlue) / 3);
-            image[i][j].rgbtGreen = round((avgAbove.rgbtGreen + avgLine.rgbtGreen + avgBelow.rgbtGreen) / 3);
-
         }
     }
-}
-
-RGBTRIPLE rowAvg(int row, int column, int width, RGBTRIPLE *copy[row + 1][column + 1])
-{
-    RGBTRIPLE image[row + 1][column + 1] = *copy;
-    RGBTRIPLE avg;
-
-    //average current pixel and right pixel if on left edge
-    if (column == 0)
-    {
-        avg.rgbtRed = round((image[row][column].rgbtRed + image[row][column + 1].rgbtRed) / 2);
-        avg.rgbtGreen = round((image[row][column].rgbtGreen + image[row][column + 1].rgbtGreen) / 2);
-        avg.rgbtBlue = round((image[row][column].rgbtBlue + image[row][column + 1].rgbtBlue) / 2);
-    }
-    //average current pixel and left pixel if on right edge
-    else if (column == width - 1)
-    {
-        avg.rgbtRed = round((image[row][column].rgbtRed + image[row][column - 1].rgbtRed) / 2);
-        avg.rgbtGreen = round((image[row][column].rgbtGreen + image[row][column - 1].rgbtGreen) / 2);
-        avg.rgbtBlue = round((image[row][column].rgbtBlue + image[row][column - 1].rgbtBlue) / 2);
-    }
-    //average current pixel and two adjacent pixels
-    else
-    {
-        avg.rgbtRed = round((image[row][column - 1].rgbtRed + image[row][column].rgbtRed + image[row][column + 1].rgbtRed) / 3);
-        avg.rgbtGreen = round((image[row][column - 1].rgbtGreen + image[row][column].rgbtGreen + image[row][column + 1].rgbtGreen) / 3);
-        avg.rgbtBlue = round((image[row][column - 1].rgbtBlue + image[row][column].rgbtBlue + image[row][column + 1].rgbtBlue) / 3);
-    }
-
-    return avg;
+    
 }
