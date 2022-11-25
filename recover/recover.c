8f8c9263559a7a7ca7ae6d6e65250d3726a0f68c
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-11-25 17:23:06 +0000

    Fri, Nov 25, 2022, 5:23 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     if(argc != 1)
     {
-        printf("Too many arguments. use ./recover {}")
+        printf("Too many arguments. use ./recover {file}")
+        return 1;
     }
 }
