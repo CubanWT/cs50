ee889ad99c8cafad7ffdd0800becc4eb72b1cde2
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-11-25 20:51:25 +0000

    Fri, Nov 25, 2022, 8:51 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,16 +3,20 @@
 
 int main(int argc, char *argv[])
 {
+    //ensure proper usage
     if(argc != 1)
     {
         printf("Too many arguments. use ./recover {file}\n");
         return 1;
     }
 
+    //read input file and check if file can't be opened
     FILE *input = fopen(argv[0], "r");
     if (input == NULL)
     {
         printf("File cannot be opened.\n");
         return 2;
     }
+
+    
 }
