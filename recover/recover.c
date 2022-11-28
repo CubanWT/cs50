204b6baf67b63d84dbe29c2dbed94c7319220a64
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-11-28 07:13:39 +0000

    Mon, Nov 28, 2022, 7:13 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -38,10 +38,10 @@ int main(int argc, char *argv[])
         count++;
         sprintf(filename, "%03i.jpg", count);
 
-       if ((int) buffer[0] == 0xff &&
-           (int) buffer[1] == 0xd8 &&
-           (int) buffer[2] == 0xff &&
-           ((int) buffer[3] >= 0xe0 || (int) buffer[3] <= 0xf0))
+       if ((char) buffer[0] == 0xff &&
+           (char) buffer[1] == 0xd8 &&
+           (char) buffer[2] == 0xff &&
+           ((char) buffer[3] >= 0xe0 || (char) buffer[3] <= 0xf0))
        {
         FILE *image = fopen(filename, "w");
         fwrite(image, 1, blockSize, buffer);
