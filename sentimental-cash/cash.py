653b306b90f1754d6094763412bbdceeb893c341
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-03 06:14:09 +0000

    Sat, Dec 3, 2022, 6:14 AM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -2,7 +2,7 @@ from cs50 import get_float
 
 # Get input
 while True:
-    usript = round(get_float("How much change? "), 2)
+    usript = get_float("How much change? ")
     if usript > 0:
         break
     print("Invalid amount of change!")
@@ -15,25 +15,24 @@ coins = {
     "pennies": 0,
 }
 
+# note: I kept getting floating point imprecision
+
 # Calculate quarters
 coins["quarters"] = usript // 0.25
-usript -= coins["quarters"] * 0.25
-print(f"quarters: {usript}")
+usript = round(usript - coins["quarters"] * 0.25, 2)
 
 # Calculate dimes
 coins["dimes"] = usript // 0.10
-usript -= coins["dimes"] * 0.10
-print(f"dimes: {usript}")
+usript = round(usript - coins["dimes"] * 0.10, 2)
 
 # Calculate nickels
 coins["nickels"] = usript // 0.05
-usript -= coins["nickels"] * 0.05
-print(f"nickels: {usript}")
+usript = round(usript - coins["nickels"] * 0.05, 2)
 
 # Calculate pennies
 coins["pennies"] = usript // 0.01
-print(f"pennies: {usript}")
 
-total = coins["pennies"] + coins["quarters"] + coins["nickels"] + coins["dimes"]
+# Sum of all coins
+total = int(coins["pennies"] + coins["quarters"] + coins["nickels"] + coins["dimes"])
 
-print(coins)
+print(total)
