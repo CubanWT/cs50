8db8deb5f9b3d042b1c4a6cd1e42d443ab8183a5
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-03 00:37:06 +0000

    Sat, Dec 3, 2022, 12:37 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,11 +1,13 @@
 from cs50 import get_int
 
+# Keep prompting user for correct input
 while True:
     input = get_int("Height: ")
     if (input > 0 and input < 9):
         break
     print("Height must be between 1 and 8 inclusive!")
 
+# print blocks and spaces
 for i in range(input):
     n = i + 1
     print(" " * (input - n), end="")
