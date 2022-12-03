2b15b988cac8184f6bcd705aaf2830a9f4510139
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-03 06:41:00 +0000

    Sat, Dec 3, 2022, 6:41 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -7,6 +7,7 @@ s = 0
 l = 0
 w = 0
 
+# Parse string for words, letters, and sentences
 for c in input.lower().lstrip():
     if c >= "a" and c <= "z":
         l += 1
@@ -15,12 +16,12 @@ for c in input.lower().lstrip():
     elif c == "." or "?" or "!":
         s += 1
 
-# Calculate readability index and print
+# Calculate readability index
 l = l / w * 100
 s = s / w * 100
 index = int(0.0588 * l - 0.296 * s - 15.8)
 
-
+#print index
 if index < 1:
     print("Before Grade 1")
 elif index > 16:
