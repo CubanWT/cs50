6370283bfe8c0250cb8a1d1efcef3c8488a5d1fe
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-04 20:26:12 +0000

    Sun, Dec 4, 2022, 8:26 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -4,8 +4,8 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    if len(sys.argv) != 2:
-        print("Incorrect usage! use dna.py {CSV data}")
+    if len(sys.argv) != 3:
+        print("Incorrect usage! use dna.py {CSV data} {DNA text file}")
     # TODO: Read database file into a variable
 
     # TODO: Read DNA sequence file into a variable
