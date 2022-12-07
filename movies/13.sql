e076af112a85178a75811247e8c6f8ac0a75319f
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-07 16:23:18 +0000

    Wed, Dec 7, 2022, 4:23 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -2,7 +2,7 @@ SELECT DISTINCT name
 FROM people
 JOIN stars ON stars.person_id = people.id
 JOIN movies ON movies.id = stars.movie_id
-WHERE title IN (
+WHERE name != "Kevin Bacon" AND title IN (
     SELECT title
     FROM movies
     JOIN stars ON movies.id = stars.movie_id
