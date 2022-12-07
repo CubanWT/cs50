126a9201b73241dd52ee931e5059480703a9524c
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-07 16:20:09 +0000

    Wed, Dec 7, 2022, 4:20 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,11 @@
-SELECT 
+SELECT name
+FROM people
+JOIN stars ON stars.person_id = people.id
+JOIN movies ON movies.id = stars.movie_id
+WHERE title IN (
+    SELECT title
+    FROM movies
+    JOIN stars ON movies.id = stars.movie_id
+    JOIN people ON people.id = stars.person_id
+    WHERE name = "Kevin Bacon" AND birth = 1958
+)
