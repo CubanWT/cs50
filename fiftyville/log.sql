7949e745e9424f718c37422373d9da73cb4b8fbf
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-07 20:57:13 +0000

    Wed, Dec 7, 2022, 8:57 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -34,16 +34,6 @@ AND minute >= 15
 AND minute <= 25
 AND activity = "exit";
 
---check ATM withdrawls from day of the robbery
-SELECT name FROM people
-JOIN bank_accounts ON people.id = bank_accounts.person_id
-JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
-WHERE atm_location = "Leggett Street"
-AND year = 2021
-AND month = 7
-AND day = 28
-AND transaction_type = "withdraw";
-
 --check calls for thief's call to associate and cross reference with people who left parking lot
 SELECT DISTINCT name from people
 JOIN phone_calls ON phone_calls.caller = people.phone_number
@@ -63,4 +53,34 @@ AND people.license_plate IN (
 );
 
 --current suspects: Sofia, Kelsey, Bruce, Diana
+
+--cross reference atm withdrawls with previous query
+SELECT name FROM people
+JOIN bank_accounts ON people.id = bank_accounts.person_id
+JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
+WHERE atm_location = "Leggett Street"
+AND year = 2021
+AND month = 7
+AND day = 28
+AND transaction_type = "withdraw"
+AND name IN (
+    SELECT DISTINCT name from people
+    JOIN phone_calls ON phone_calls.caller = people.phone_number
+    WHERE year = 2021
+    AND month = 7
+    AND day = 28
+    AND duration < 60
+    AND people.license_plate IN (
+        SELECT license_plate FROM bakery_security_logs
+        WHERE year = 2021
+        AND month = 7
+        AND day = 28
+        AND hour = 10
+        AND minute >= 15
+        AND minute <= 25
+        AND activity = "exit"
+    )
+);
+
+--current suspects: Bruce, Diana
 
