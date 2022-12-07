cf437d5b04e01d4f4a4a0010911dc54848f9df9f
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-07 00:06:45 +0000

    Wed, Dec 7, 2022, 12:06 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -4,7 +4,7 @@ JOIN stars ON movies.id = stars.movie_id
 JOIN people ON people.id = stars.person_id
 WHERE  movies.id = (
     SELECT movie_id
-    FROM movies
+    FROM stars
     WHERE 
 )
 AND name = "Helena Bonham Carter";
