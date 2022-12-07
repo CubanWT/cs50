ef0f51324d8dafcaf57fd77a19e4be32f37112f5
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-07 21:54:41 +0000

    Wed, Dec 7, 2022, 9:54 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -103,7 +103,30 @@ WHERE year = 2021
 AND month = 7
 AND day = 29
 AND name IN (
-    SELECT name from people
-    WHERE name = "Bruce"
-    OR name = "Diana"
+    SELECT name FROM people
+JOIN bank_accounts ON people.id = bank_accounts.person_id
+JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
+WHERE atm_location = "Leggett Street"
+AND year = 2021
+AND month = 7
+AND day = 28
+AND transaction_type = "withdraw"
+AND name IN (
+    SELECT DISTINCT name from people
+    JOIN phone_calls ON phone_calls.caller = people.phone_number
+    WHERE year = 2021
+    AND month = 7
+    AND day = 28
+    AND duration < 60
+    AND people.license_plate IN (
+        SELECT license_plate FROM bakery_security_logs
+        WHERE year = 2021
+        AND month = 7
+        AND day = 28
+        AND hour = 10
+        AND minute >= 15
+        AND minute <= 25
+        AND activity = "exit"
+    )
+);
 );
