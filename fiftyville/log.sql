5e59ce8e11820800d476f0dfb3ccce53c24a2600
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-07 22:25:01 +0000

    Wed, Dec 7, 2022, 10:25 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -74,7 +74,7 @@ WHERE flight_id = (
     AND day = 29
     ORDER BY hour ASC
     LIMIT 1
-)
+);
 
 --find suspect
 SELECT name FROM people
@@ -90,28 +90,32 @@ WHERE flight_id = (
     ORDER BY hour ASC
     LIMIT 1
 )
-)
 AND name IN (
-    SELECT full_name, city FROM airports
-WHERE id = (
-    SELECT destination_airport_id FROM flights
+    SELECT name from people
+    JOIN phone_calls ON phone_calls.caller = people.phone_number
     WHERE year = 2021
     AND month = 7
-    AND day = 29
-    ORDER BY hour ASC
-    LIMIT 1
-);
+    AND day = 28
+    AND duration < 60
 )
 AND name IN (
     SELECT name FROM people
-JOIN bank_accounts ON people.id = bank_accounts.person_id
-JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
-WHERE atm_location = "Leggett Street"
-AND year = 2021
-AND month = 7
-AND day = 28
-AND transaction_type = "withdraw";
+    JOIN bank_accounts ON people.id = bank_accounts.person_id
+    JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
+    WHERE atm_location = "Leggett Street"
+    AND year = 2021
+    AND month = 7
+    AND day = 28
+    AND transaction_type = "withdraw"
 )
 AND name IN (
-    
+    SELECT people.name FROM bakery_security_logs
+    JOIN people ON people.license_plate = bakery_security_logs.license_plate
+    WHERE year = 2021
+    AND month = 7
+    AND day = 28
+    AND hour = 10
+    AND minute >= 15
+    AND minute <= 25
+    AND activity = "exit"
 )
