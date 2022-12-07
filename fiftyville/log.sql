dc5141e32444442c69150c2142def861ec708008
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-07 19:50:15 +0000

    Wed, Dec 7, 2022, 7:50 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -64,4 +64,10 @@ AND day = 28
 AND duration < 60;
 
 --check bakery security footage
-SELECT
+SELECT license_plate FROM bakery_security_logs
+WHERE year = 2021
+AND month = 7
+AND day = 28
+AND hour = 10
+AND minute >= 15
+AND minute <= 25;
