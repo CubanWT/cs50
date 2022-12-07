8ac0b9af425d762d20547fdf9a95a2bf4e37cdb3
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-07 20:00:48 +0000

    Wed, Dec 7, 2022, 8:00 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -74,4 +74,19 @@ AND minute <= 25;
 
 --get people who's license plate coincides with numbers who called after robbery
 SELECT name FROM people
-JOIN people
+WHERE phone_number IN (
+    SELECT receiver FROM phone_calls
+    WHERE year = 2021
+    AND month = 7
+    AND day = 28
+    AND duration < 60
+)
+AND license_plate IN (
+    SELECT license_plate FROM bakery_security_logs
+    WHERE year = 2021
+    AND month = 7
+    AND day = 28
+    AND hour = 10
+    AND minute >= 15
+    AND minute <= 25
+);
