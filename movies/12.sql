4787b8bc7663a2b0c61f7b557d6f9b09d50defdd
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-07 00:48:50 +0000

    Wed, Dec 7, 2022, 12:48 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -2,5 +2,5 @@ SELECT title
 FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
-WHERE "Johnny Depp" IN name
-AND "Helena Bonham Carter" IN name;
+WHERE "Johnny Depp" IN (SELECT name FROM people)
+AND "Helena Bonham Carter" IN (SELECT name FROM people);
