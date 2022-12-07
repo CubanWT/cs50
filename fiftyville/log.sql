6c13f563d33193f7b9d297bf88218d9157621b45
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-07 17:39:13 +0000

    Wed, Dec 7, 2022, 5:39 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -19,3 +19,7 @@ WHERE id = 295) AND
 day = (SELECT day
 FROM crime_scene_reports
 WHERE id = 295);
+
+--thief plans on taking the earliest flight on 29/7/2021
+--thief withdrew money on the  of 28/7/2021 from ATM on Leggett Street
+--thief left in a car seen by bakery security footage within ten minutes of crime (10:15 AM)
