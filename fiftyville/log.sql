d6ed641701f55eade8963a212e56980c6d0d7246
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-07 22:43:13 +0000

    Wed, Dec 7, 2022, 10:43 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -119,11 +119,59 @@ AND name IN (
     AND minute >= 15
     AND minute <= 25
     AND activity = "exit"
-)
+);
 
 --find associate
 SELECT name FROM people
 JOIN phone_calls ON phone_calls.receiver = people.phone_number
 WHERE caller = (
-    SELECT 
-)
+    SELECT phone_number FROM people
+    WHERE year = 2021
+    AND month = 7
+    AND 
+    AND name IN (
+        SELECT name FROM people
+        WHERE name IN (
+            SELECT name FROM people
+            JOIN passengers ON passengers.passport_number = people.passport_number
+            JOIN flights ON flights.id = passengers.flight_id
+            WHERE flight_id = (
+                SELECT id FROM flights
+                WHERE year = 2021
+                AND month = 7
+                AND day = 29
+                ORDER BY hour ASC
+                LIMIT 1
+            )
+        )
+        AND name IN (
+            SELECT name from people
+            JOIN phone_calls ON phone_calls.caller = people.phone_number
+            WHERE year = 2021
+            AND month = 7
+            AND day = 28
+            AND duration < 60
+        )
+        AND name IN (
+            SELECT name FROM people
+            JOIN bank_accounts ON people.id = bank_accounts.person_id
+            JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
+            WHERE atm_location = "Leggett Street"
+            AND year = 2021
+            AND month = 7
+            AND day = 28
+            AND transaction_type = "withdraw"
+        )
+        AND name IN (
+            SELECT people.name FROM bakery_security_logs
+            JOIN people ON people.license_plate = bakery_security_logs.license_plate
+            WHERE year = 2021
+            AND month = 7
+            AND day = 28
+            AND hour = 10
+            AND minute >= 15
+            AND minute <= 25
+            AND activity = "exit"
+        )
+    )
+);
