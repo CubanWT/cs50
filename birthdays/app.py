ba3512c7b6f7958174a880e97b2baaf36b1cd50d
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-20 21:39:26 +0000

    Tue, Dec 20, 2022, 9:39 PM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -32,7 +32,9 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
+        entrys = db.execute("SELECT * FROM birthdays")
 
-        return render_template("index.html")
+
+        return render_template("index.html", entrys=entrys)
 
 
