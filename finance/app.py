3444b6546bc3d0256cad47ed980919c6da2151c6
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-21 05:20:46 +0000

    Wed, Dec 21, 2022, 5:20 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -63,7 +63,7 @@ def buy():
         if not shares > 0:
             return apology("Invalid number of shares")
 
-        db.execute("CREATE TABLE IF NOT EXISTS transactions")
+        db.execute("CREATE TABLE IF NOT EXISTS transactions ( user_id INTEGER PRIMARY KEY")
 
     return render_template("buy.html")
 
