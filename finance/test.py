38e50b2c10b50f4d97d650b6fcf532b3904ae2f9
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-21 04:32:08 +0000

    Wed, Dec 21, 2022, 4:32 AM +00:00

--- finance/test.py
+++ finance/test.py
@@ -2,6 +2,10 @@ from cs50 import SQL
 
 db = SQL("sqlite:///finance.db")
 
+name = "willeupi"
+
 test_list = db.execute("SELECT username FROM users")
 
+if name in test_list["username"]:
+    print(name)
 print(test_list)
