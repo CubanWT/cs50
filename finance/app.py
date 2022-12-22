2fef78da1ac108384178e4c2064c64f462210bdb
CubanWT <36120266+CubanWT@users.noreply.github.com>
2022-12-22 02:14:19 +0000

    Thu, Dec 22, 2022, 2:14 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -245,7 +245,7 @@ def sell():
 
         db.execute("UPDATE stocks SET shares = ? WHERE user_id = ? AND symbol = ?", user_stock["shares"], user_id, user_stock["symbol"])
         db.execute("UPDATE users SET cash = ? WHERE id = ?", user_cash, user_id)
-        db.execute("INSERT INTO transactions (user_id, time, type, symbol, shares, price, total) VALUES (?, ?, 'SELL', ?, ?, ?)", user_id, time, user_stock["symbol"], shares, user_stock["price"], value)
+        db.execute("INSERT INTO transactions (user_id, time, type, symbol, shares, price, total) VALUES (?, ?, 'SELL', ?, ?, ?, ?)", user_id, time, user_stock["symbol"], shares, user_stock["price"], value)
         return redirect("/")
 
     return render_template("sell.html", stocks=stocks)
